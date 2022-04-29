2d61859010b6166fe894bf9020350be28842744a
acontrerasregueiro <acontrerasregueiro@gmail.com>
2022-04-29 11:43:34 +0000

    ejemplo setEffect y setStatus

--- src/js/component/app.jsx
+++ src/js/component/app.jsx
@@ -3,6 +3,13 @@ import React, { useState, useEffect } from "react";
 //create your first component
 const App = () => {
 	const [tarea, setTarea] = useState("");
+	const [lista, setLista] = useState([]);
+	// CUANDO HAYA UN CAMBIO EN LISTA, ME MUESTRA POR CONSOLA LISTA
+
+	useEffect(() => {
+		console.log(lista);
+	}, [lista]);
+
 	return (
 		<div>
 			<h1 className="text-center mt-5"> TODO LIST</h1>
@@ -15,7 +22,15 @@ const App = () => {
 				onClick={(e) => {
 					alert(tarea);
 				}}>
-				Añadir
+				Añadir TAREA
+			</button>
+			<button
+				onClick={(e) => {
+					//agrega lista a las tareas que ya teniamos
+					setLista([...lista, tarea]);
+					console.log;
+				}}>
+				Añadir a lista
 			</button>
 		</div>
 	);
